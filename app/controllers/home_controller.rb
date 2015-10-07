faa7ad598ceb2bbf2c80174ed3c4633d3edc68d2
Edgar Lee <edgarhinshunlee@gmail.com>
2015-10-07 11:32:17 -0400

    Scaffold out photo model and display in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    photos = F00px.get('photos').body
-    render json: photos
+    response = F00px.get('photos')
+    json = JSON.parse(response.body)
+    @photos = json['photos'].map do |photo|
+      Photo.new photo['id'], photo['name'], photo['image_url']
+    end
   end
 end
